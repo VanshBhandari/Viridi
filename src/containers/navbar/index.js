4cefad3590ec436623ee2dacb2ff44aa4658de3e
Jeewoo Chung <awesomejwc@icloud.com>
2022-03-26 13:12:58 -0700

    DARKEN THE F ING BACKGROUND

--- src/containers/navbar/index.js
+++ src/containers/navbar/index.js
@@ -18,26 +18,28 @@ export default function Navbar() {
 
     return (
         <>
-            <nav className="NavBar flex justify-around py-4 mx-auto bg-accent w-96 rounded-b-2xl">
-                <div className="navicons flex flex-row gap-12">
-                    <div className="CreatePost">
-                        <p><AiOutlinePlusCircle size={28} /></p>
-                    </div>
-                    <div className="Activity">
-                        <p><AiOutlineHeart size={28} /></p>
-                    </div>
-                    <div className="Location">
-                        <p><MdOutlineLocationOn size={28} /></p>
-                    </div>
-                    <button className="Shop" onClick={OnShopClick}>
-                        <p><MdOutlineShoppingCart size={28} /></p>
-                    </button>
-                </div>
-            </nav>
+            <div className="flex flex-row justify-around">
+                <></>
+                <nav className="NavBar flex justify-around py-4 mx-auto bg-accent w-96 rounded-b-2xl">
+                    <div className="navicons flex flex-row gap-12">
+                        <div className="CreatePost">
+                            <p><AiOutlinePlusCircle size={28} /></p>
+                        </div>
+                        <div className="Activity">
+                            <p><AiOutlineHeart size={28} /></p>
+                        </div>
+                        <div className="Location">
+                            <p><MdOutlineLocationOn size={28} /></p>
+                        </div>
+                        <button className="Shop" onClick={OnShopClick}>
+                            <p><MdOutlineShoppingCart size={28} /></p>
+                            <Shop trigger={isShopClick} setTrigger={setIsShopClick}>
 
-            <Shop trigger={isShopClick} setTrigger={setIsShopClick}>
-
-            </Shop>
+                            </Shop>
+                        </button>
+                    </div>
+                </nav>
+            </div>
         </>
     )
 
--- src/containers/profile/index.js
+++ src/containers/profile/index.js
@@ -1,38 +1,41 @@
 import React, { useState } from 'react'
-import { CgProfile } from 'react-icons/cg'
+import { FaAngellist } from "react-icons/fa";
 import './style.css'
 
 export default function Profile() {
     const [isProfileOpen, setIsProfileOpen] = useState(false)
     
     return (!isProfileOpen) ? (
-        <div className='profile'>
+        <div className='profile mt-5'>
             <button className='profbtn' onClick={()=> setIsProfileOpen(true)}>
-                <p><CgProfile size={50} /></p>
+                <p><FaAngellist size={50} /></p>
             </button>
         </div>
     ) : (
         <>
-            <div className='profile'>
-                <button className='profbtn' onClick={()=> setIsProfileOpen(false)}>
-                    <p><CgProfile size={50}  /></p>
-                </button>
-            </div>
-            <div className='profpop' >
-                <div className='username'>                   
-                    <p>Full Name</p>
+            <div className='overlay o-block'></div>
+            <div className='popup'>
+                <div className='profile'>
+                    <button className='profbtn' onClick={()=> setIsProfileOpen(false)}>
+                        <p><FaAngellist size={50}  /></p>
+                    </button>
                 </div>
-                <div className='numberofposts'>
-                    <p>3 posts</p>
-                </div>
-                <div className='treesplanted'>
-                    <p>1 tree planted</p>
-                </div>
-                <div className='rank'>
-                    <p>Rank:192</p>
-                </div>
-                <div className='description'>
-                    <p>wannabe environmentalist. used to annoy people on fb now trying to annoy people here. #gotrees #bi #vegan #pride</p>
+                <div className='profpop' >
+                    <div className='username'>                   
+                        <p>Full Name</p>
+                    </div>
+                    <div className='numberofposts'>
+                        <p>3 posts</p>
+                    </div>
+                    <div className='treesplanted'>
+                        <p>1 tree planted</p>
+                    </div>
+                    <div className='rank'>
+                        <p>Rank:192</p>
+                    </div>
+                    <div className='description'>
+                        <p>wannabe environmentalist. used to annoy people on fb now trying to annoy people here. #gotrees #bi #vegan #pride</p>
+                    </div>
                 </div>
             </div>
         </>
