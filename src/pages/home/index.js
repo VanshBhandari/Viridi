d1cad2ebb2f4f44cd413f118db66f08b23cae1dc
val-hir <jinpark11251125@gmail.com>
2022-03-25 18:27:00 -0700

    .

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import "./style.css"
-import { Navbar, SidePanel } from '../../containers'
+import { Navbar, SidePanel, signInWithGoogle } from '../../containers'
 
 export default function Home() {
   return (
     <div className="home">
+        <signInWithGoogle />
         <SidePanel />
         <Navbar />
     </div>
