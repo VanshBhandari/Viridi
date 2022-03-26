49da2b5fdcaa258df69900aa08d8415d1c23f8ff
Jeewoo Chung <awesomejwc@icloud.com>
2022-03-25 23:24:37 -0700

    ignore this

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,12 +4,8 @@ import { MainSection, SidePanel, Profile } from '../../containers'
 
 export default function Home() {
   return (
-<<<<<<< Updated upstream
     <div className="home flex min-h-screen overflow-y-auto">
         <Profile />
-=======
-    <div className="flex min-h-screen overflow-y-auto">
->>>>>>> Stashed changes
         <SidePanel />
         <MainSection />
     </div>
