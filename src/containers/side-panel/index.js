f3607b0d0dd7db36dc5bbf3c7f6d3441dd885f19
vansh <71205949+VanshBhandari@users.noreply.github.com>
2022-03-25 20:50:36 -0700

    small changes to side panel

--- src/containers/side-panel/index.js
+++ src/containers/side-panel/index.js
@@ -3,7 +3,7 @@ import './style.css'
 
 export default function SidePanel() {
   return (
-    <div className='sidepanel w-60 h-full shadow-md bg-accent px-1 absolute'>
+    <div className='sidepanel w-64 h-full shadow-md bg-accent px-1 absolute'>
        <img src={Logo} className="logo"/>
        <p className='title'>VIRIDI</p>
     </div>
